e9ea9d260a37d8480227305f5f8c629ad692ba80
Henrique Daniel Luvison <141172868+henriquevison@users.noreply.github.com>
2023-10-04 21:46:44 -0300

    first commit

---
+++ menu.js
@@ -0,0 +1,15 @@
+window.addEventListener("scroll",function(){
+    let header = document.querySelector('#header')
+    header.classList.toggle('rolagem',window.scrollY > 0)
+})
+
+// menu responsivo
+function menuShow(){
+    let menuMobile = document.querySelector(".mobile_menu");
+    
+    if (menuMobile.classList.contais("open")){
+        menuMobile.classList.remove("open");
+    }else {
+        menuMobile.classList.add("open");
+    }
+}
