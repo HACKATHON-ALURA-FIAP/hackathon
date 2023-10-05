a47959d63e171007ef254ffff8bee7cf32f5649a
unknown <henriquedanielluvison@gmail.com>
2023-10-05 15:54:51 -0300

    formatação do codigo

--- menu.js
+++ menu.js
@@ -1,15 +1,15 @@
-window.addEventListener("scroll",function(){
-    let header = document.querySelector('#header')
-    header.classList.toggle('rolagem',window.scrollY > 0)
-})
+window.addEventListener("scroll", function () {
+  let header = document.querySelector("#header");
+  header.classList.toggle("rolagem", window.scrollY > 0);
+});
 
 // menu responsivo
-function menuShow(){
-    let menuMobile = document.querySelector(".mobile_menu");
-    
-    if (menuMobile.classList.contais("open")){
-        menuMobile.classList.remove("open");
-    }else {
-        menuMobile.classList.add("open");
-    }
+function menuShow() {
+  let menuMobile = document.querySelector(".mobile_menu");
+
+  if (menuMobile.classList.contais("open")) {
+    menuMobile.classList.remove("open");
+  } else {
+    menuMobile.classList.add("open");
+  }
 }
